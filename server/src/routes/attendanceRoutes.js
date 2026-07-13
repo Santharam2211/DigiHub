fd6604813fb61bfd4099576a25cf0337c36b26b6
Santharam2211 <santharamsenthil57@gmail.com>
2026-07-13 20:40:41 +0530

    Update attendanceRoutes.js

--- server/src/routes/attendanceRoutes.js
+++ server/src/routes/attendanceRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { markAttendance, getAttendanceReport, getAttendanceRecords, exportReport, exportPDFReport } = require('../controllers/attendanceController');
-const { protect } = require('../middlewares/authMiddleware');
+const { protect, authorize } = require('../middlewares/authMiddleware');
 
-router.post('/mark', protect, markAttendance);
-router.get('/report/:eventId', protect, getAttendanceReport);
-router.get('/records/:eventId', protect, getAttendanceRecords);
-router.get('/export/:eventId', protect, exportReport);
-router.post('/export-pdf/:eventId', protect, exportPDFReport);
+router.post('/mark', protect, authorize('Admin', 'Association Member', 'Class Coordinator', 'Program Coordinator', 'Faculty', 'Volunteer'), markAttendance);
+router.get('/report/:eventId', protect, authorize('Admin', 'Association Member', 'Faculty'), getAttendanceReport);
+router.get('/records/:eventId', protect, authorize('Admin', 'Association Member', 'Faculty'), getAttendanceRecords);
+router.get('/export/:eventId', protect, authorize('Admin', 'Association Member', 'Faculty'), exportReport);
+router.post('/export-pdf/:eventId', protect, authorize('Admin', 'Association Member', 'Faculty'), exportPDFReport);
 
 module.exports = router;
